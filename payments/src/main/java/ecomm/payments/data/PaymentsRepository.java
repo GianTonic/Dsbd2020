ee010c1f4c557cb90a7c252f9387be9b40f2f435
sartoro94@gmail.com <you@example.com>
2020-12-28 12:35:58 +0100

    Add Kafka service and clean code

--- payments/src/main/java/ecomm/payments/data/PaymentsRepository.java
+++
@@ -1,9 +0,0 @@
-package ecomm.payments.data;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import payments.Payments;
-
-public interface PaymentsRepository extends MongoRepository<Payments, String> {
-
-    //
-}
